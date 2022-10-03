a55690d62e3610cf22b45b23e4aff2d2bd5e165a
Clayton Rogers <claytonrogers53@gmail.com>
2022-10-02 23:42:49 -0400

    Fix weather to use city ID

--- src/weather.cpp
+++ src/weather.cpp
@@ -1,43 +1,47 @@
 #include "weather.h"
 
-
-#include <HTTPClient.h>
 #include <Arduino_JSON.h>
+#include <HTTPClient.h>
 
 #include "API_KEYS.h"
 
-
-//weather api
-#define OPENWEATHERMAP_URL "http://api.openweathermap.org/data/2.5/weather?q="
-#define TEMP_UNIT "metric" //use "imperial" for Fahrenheit"
-
+// weather api
+#define OPENWEATHERMAP_URL "http://api.openweathermap.org/data/2.5/weather?"
+#define TEMP_UNIT "metric"  // use "imperial" for Fahrenheit"
 
 RTC_DATA_ATTR WeatherData currentWeather;
+RTC_DATA_ATTR uint16_t fail_count = 0;
 
-bool update_weather_data_from_internet(){
+bool update_weather_data_from_internet() {
     HTTPClient http;
     bool success = false;
-    http.setConnectTimeout(3000);//3 second max timeout
-    String weatherQueryURL = String(OPENWEATHERMAP_URL) + String(CITY_ID) + String("&units=") + String(TEMP_UNIT) + String("&appid=") + String(OPENWEATHERMAP_APIKEY);
+    http.setConnectTimeout(3000);  // 3 second max timeout
+    String weatherQueryURL = String(OPENWEATHERMAP_URL) + String("id=") +
+                             String(CITY_ID) + String("&units=") +
+                             String(TEMP_UNIT) + String("&appid=") +
+                             String(OPENWEATHERMAP_APIKEY);
     http.begin(weatherQueryURL.c_str());
     int httpResponseCode = http.GET();
     if (httpResponseCode == 200) {
         String payload = http.getString();
         JSONVar responseObject = JSON.parse(payload);
-        currentWeather.temperature = int(responseObject["main"]["temp"]);
-        currentWeather.weather_condition_code = int(responseObject["weather"][0]["id"]);
+        currentWeather.temperature = int(responseObject["main"]["temp_max"]);
+        currentWeather.weather_condition_code =
+            int(responseObject["weather"][0]["id"]);
         success = true;
+        fail_count = 0;
     } else {
-        //http error
-        currentWeather.temperature = 0;
-        currentWeather.weather_condition_code = 800;
+        // http error
         success = false;
+        ++fail_count;
+        if (fail_count > 10) {
+            currentWeather.temperature = 0;
+            currentWeather.weather_condition_code = 800;
+        }
     }
     http.end();
 
     return success;
 }
 
-WeatherData get_weather_data() {
-    return currentWeather;
-}
+WeatherData get_weather_data() { return currentWeather; }
