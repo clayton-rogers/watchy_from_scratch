50290696619126e6d46cfda237ccfb8eb83b5005
Clayton Rogers <claytonrogers53@gmail.com>
2022-10-02 23:07:58 -0400

    Add API key template

---
+++ include/API_KEYS_TEMPLATE.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define GOOGLE_CALENDAR_REFRESH_TOKEN "foobar"
+#define GOOGLE_CALENDAR_API_KEY "foobar"
+#define GOOGLE_CALENDAR_CLIENT_ID "foobar"
+#define GOOGLE_CALENDAR_CLIENT_SECRET "foobar"
+
+
+#define WIFI_SSID "MyInternet"
+#define WIFI_PASSWORD "passwprd"
+
+#define CITY_ID "city_id" // enter your own
+#define OPENWEATHERMAP_APIKEY "foobar" // use your own API key :)
